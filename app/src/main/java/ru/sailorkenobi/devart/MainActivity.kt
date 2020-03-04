3ef6beb998b507068216ce3f98122c307b7c18ec
Max Kartsev <sailorKenobi@gmail.com>
2020-03-04 23:53:24 +0300

    blank recent fragment

--- app/src/main/java/ru/sailorkenobi/devart/MainActivity.kt
+++ app/src/main/java/ru/sailorkenobi/devart/MainActivity.kt
@@ -8,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = this.supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val recentFragment = RecentFragment()
+        fragmentTransaction.replace(R.id.main_fragment_container, recentFragment)
+        fragmentTransaction.commit()
     }
 }

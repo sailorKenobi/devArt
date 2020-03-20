aaada6d95f6cfae63101fcc512b01c2d93270e41
Max Kartsev <sailorKenobi@gmail.com>
2020-03-20 17:24:43 +0300

    bottom navigation menu

--- app/src/main/java/ru/sailorkenobi/devart/MainActivity.kt
+++ app/src/main/java/ru/sailorkenobi/devart/MainActivity.kt
@@ -1,14 +1,43 @@
 package ru.sailorkenobi.devart
 
-import androidx.appcompat.app.AppCompatActivity
+//import android.R
+import ru.sailorkenobi.devart.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.sailorkenobi.devart.R.layout.activity_main
 
-class MainActivity : SingleFragmentActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun createFragment(): Fragment {
-        //return GalleryFragment()
-        return RecentFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+
+        navigationView.setOnNavigationItemSelectedListener OnNavigationItemSelectedListener@{ menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_newest -> {
+                    val fragment = RecentFragment()
+                    supportFragmentManager.beginTransaction().replace(container.id, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.action_newest -> {
+                    val fragment = RecentFragment()
+                    supportFragmentManager.beginTransaction().replace(container.id, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.action_newest -> {
+                    val fragment = RecentFragment()
+                    supportFragmentManager.beginTransaction().replace(container.id, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false }
     }
-
 }
